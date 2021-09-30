2dcb3761cec58b81df74b865f5b99438714f87c5
George <71434803+george-swift@users.noreply.github.com>
2021-09-30 12:32:27 +0100

    Extend styled components

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,22 +1,61 @@
 import styled from 'styled-components';
 
+type AddButtonProps = {
+  dark?: boolean
+}
+interface DragPreviewContainerProps {
+  isHidden?: boolean
+  isPreview?: boolean
+}
+
+type DragPreviewWrapperProps = {
+  position: {
+    x: number
+    y: number
+  }
+}
+
+export const DragPreviewContainer = styled.div<DragPreviewContainerProps>`
+  transform: ${props => (props.isPreview ? "rotate(5deg)" : undefined)};
+  opacity: ${props => (props.isHidden ? 0 : 1)};
+`
+
+export const DragPreviewWrapper = styled.div.attrs<DragPreviewWrapperProps>(
+  ({ position: { x, y } }) => ({
+    style: {
+      transform: `translate(${x}px, ${y}px)`
+    }
+  })
+)<DragPreviewWrapperProps>``
+
 export const AppContainer = styled.div`
   display: flex;
-  align-items: flex-start;
+  flex-direction: column;
+  align-items: center;
   height: 100%;
   width: 100%;
   padding: 20px;
-  background-color: #3179ba;
+
+  @media (min-width: 992px) {
+    flex-direction: row;
+    align-items: flex-start;
+  }
 `
 
-export const ColumnContainer = styled.div`
+export const ColumnContainer = styled(DragPreviewContainer)`
   width: 300px;
+  margin-bottom: 1.5rem;
   min-height: 50px;
-  margin-right: 20px;
   border-radius: 3px;
-  padding: 0.5rem;
+  padding: 8px;
   flex-grow: 0;
   background-color: #ebecf0;
+  font-family: 'Poppins', sans-serif;
+
+  @media (min-width: 992px) {
+    margin-right: 20px;
+    margin-bottom: 0;
+  }
 `
 
 export const ColumnTitle = styled.div`
@@ -24,19 +63,17 @@ export const ColumnTitle = styled.div`
   font-weight: bold;
 `
 
-export const CardContainer = styled.div`
+export const CardContainer = styled(DragPreviewContainer)`
   max-width: 300px;
   margin-bottom: 0.5rem;
   padding: 0.5rem 1rem;
   background-color: #fff;
+  font-family: 'Lato', sans-serif;
+  color: #34495e;
   cursor: pointer;
   border-radius: 3px;
   box-shadow: #091e4240 0 1px 0 0;
 `
-
-type AddButtonProps = {
-  dark?: boolean
-}
 
 export const AddItemButton = styled.button<AddButtonProps>`
   width: 100%;
@@ -44,6 +81,7 @@ export const AddItemButton = styled.button<AddButtonProps>`
   padding: 10px 12px;
   text-align: left;
   background-color: #ffffff3d;
+  font-size: 14px;
   border-radius: 3px;
   border: none;
   color: ${props => (props.dark ? "#000" : "#fff")};
@@ -80,4 +118,14 @@ export const NewItemInput = styled.input`
   box-shadow: #091e4240 0 1px 0 0;
   margin-bottom: 0.5rem;
   padding: 0.5rem 1rem;
+`
+
+export const CustomDragLayerContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+  z-index: 100;
 `
