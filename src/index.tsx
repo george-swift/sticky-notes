47849b1d25e4e5c3840e39fc9a84fa1904215cdc
George <71434803+george-swift@users.noreply.github.com>
2021-09-30 12:35:44 +0100

    Wrap app in drag and drop provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheets/index.css';
 import App from './components/App';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { AppStateProvider } from './context/AppStateContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DndProvider backend={HTML5Backend}>
+      <AppStateProvider>
+        <App />
+      </AppStateProvider>
+    </DndProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
